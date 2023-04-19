5ae517b229bb0dfacc73a2d64909e9d675833a1e
Łukasz Kos <kos.lukasz.88@gmail.com>
2023-04-19 22:14:15 +0200

    Window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+bool handle_events() {
+    SDL_Event e;
+    while( SDL_PollEvent( &e ) != 0 ) {
+        switch (e.type) {
+            case SDL_QUIT:
+                return false;
+        }
+    }
+    return true;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_EVERYTHING );
+    auto window = SDL_CreateWindow( "Pinball",
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    640, 480,
+                                    SDL_WINDOW_SHOWN );
+    while (handle_events()) {
+        SDL_Delay(10);
+    }
+
+
+    SDL_DestroyWindow( window );
+    SDL_Quit();
     return 0;
 }
