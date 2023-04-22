76fe45f8bf53f18bdabaa6da6761877ed397cc00
Łukasz Kos <kos.lukasz.88@gmail.com>
2023-04-22 22:09:17 +0200

    player movement

--- main.cpp
+++ main.cpp
@@ -5,22 +5,38 @@
 #include <tuple>
 #include <vector>
 #include <iostream>
+#include <array>
 
-bool handle_events(SDL_Rect &rect) {
+
+using vec2d = std::array<double, 2>;
+
+vec2d operator+(vec2d a, vec2d b) {
+    return {a[0]+b[0], a[1]+b[1]};
+}
+
+class player_c {
+public:
+    double angle;
+    vec2d position;
+};
+
+bool handle_events(player_c &player) {
     SDL_Event e;
     auto *key_state = SDL_GetKeyboardState(nullptr);
     while( SDL_PollEvent( &e ) != 0 ) {
         switch (e.type) {
             case SDL_QUIT:
                 return false;
-
         }
     }
 
-    if (key_state[SDL_SCANCODE_UP]) rect.y -= 8;
-    if (key_state[SDL_SCANCODE_DOWN]) rect.y += 8;
-    if (key_state[SDL_SCANCODE_LEFT]) rect.x -= 8;
-    if (key_state[SDL_SCANCODE_RIGHT]) rect.x += 8;
+    if (key_state[SDL_SCANCODE_UP]) {
+        vec2d forward vec = angle_to_vector(player.angle):
+                player.position = player.position +
+    }
+//    if (key_state[SDL_SCANCODE_DOWN]) rect.y += 8;
+    if (key_state[SDL_SCANCODE_LEFT]) player.angle-=M_PI/10.0;
+    if (key_state[SDL_SCANCODE_RIGHT]) player.angle+=M_PI/10.0;
     return true;
 }
 
@@ -58,42 +74,61 @@ std::shared_ptr<SDL_Texture> load_texture (std::shared_ptr<SDL_Renderer> rendere
     return std::shared_ptr<SDL_Texture>(texture, [](auto *p) { SDL_DestroyTexture(p); });
 }
 
-int main() {
-    SDL_Init(SDL_INIT_EVERYTHING );
-    {
-        auto [window_p, renderer_p] = create_context();
+std::pair<int, int> get_texture_w_h(std::shared_ptr<SDL_Texture> texture) {
+    int w, h;
+    SDL_QueryTexture(texture.get(), nullptr, nullptr, &w, &h);
+    return {w, h};
+}
 
-        auto clouds = load_texture(renderer_p, "clouds.bmp");
-        auto background = load_texture(renderer_p, "background.bmp");
+SDL_Rect get_texture_rect(std::shared_ptr<SDL_Texture> texture) {
+    auto [w, h] = get_texture_w_h(texture);
+    return {0, 0, w, h};
+}
 
-        SDL_Rect rect = {10, 10, 300, 100};
 
-        auto prev_ticks = SDL_GetTicks();
-        int frame_dropped = 0;
-        while (handle_events(rect)) {
-            if (!frame_dropped) {
+void play_the_game(std::shared_ptr<SDL_Renderer> renderer_p) {
+    auto clouds = load_texture(renderer_p, "clouds.bmp");
+    auto background = load_texture(renderer_p, "background.bmp");
+    auto player_texture = load_texture(renderer_p, "player1.bmp");
+    SDL_Rect player_rect = get_texture_rect(player_texture);
+    player_c player = {0, {320.0, 200.0}};
+
+
+    auto prev_ticks = SDL_GetTicks();
+    int frame_dropped = 0;
+    while (handle_events(player)) {
+        if (!frame_dropped) {
 //                SDL_SetRenderDrawColor(renderer_p.get(), 0, 0, 0, 255);
 //                SDL_RenderClear(renderer_p.get());
-                SDL_RenderCopy(renderer_p.get(), background.get(), nullptr, nullptr);
-                {
-                    int w;
-                    int h;
-                    SDL_QueryTexture(clouds.get(), NULL, NULL, &w, &h);
-                    SDL_Rect clouds_rect = { rect.x / 2 - 200, rect.y / 2 - 200, w, h};
-                    SDL_RenderCopy(renderer_p.get(), clouds.get(), nullptr, &clouds_rect);
-                }
-                SDL_SetRenderDrawColor(renderer_p.get(), 255, 100, 50, 255);
-                SDL_RenderFillRect(renderer_p.get(), &rect);
-                SDL_RenderPresent(renderer_p.get());
+            SDL_RenderCopy(renderer_p.get(), background.get(), nullptr, nullptr);
+            {
+                auto [w, h] = get_texture_w_h(clouds);
+                SDL_Rect clouds_rect = { player_rect.x / 2 - 200, player_rect.y / 2 - 200, w, h};
+                SDL_RenderCopy(renderer_p.get(), clouds.get(), nullptr, &clouds_rect);
             }
-            auto ticks = SDL_GetTicks();
-            if ((ticks - prev_ticks) < 33) {
-                SDL_Delay(33 - (ticks - prev_ticks));
-                frame_dropped = 0;
-            } else { frame_dropped = 1; }
-            prev_ticks += 33;
+            auto rect = player_rect;
+            rect.x = player.position[0] - rect.w/2;
+            rect.y = player.position[1] - rect.h/2;
+
+            SDL_RenderCopyEx(renderer_p.get(), player_texture.get(), nullptr, &rect, 180.0*player.angle/M_PI, nullptr, SDL_FLIP_NONE);
+//            SDL_SetRenderDrawColor(renderer_p.get(), 255, 100, 50, 255);
+//            SDL_RenderFillRect(renderer_p.get(), &player_rect);
+            SDL_RenderPresent(renderer_p.get());
         }
+
+        auto ticks = SDL_GetTicks();
+        if ((ticks - prev_ticks) < 33) {
+            SDL_Delay(33 - (ticks - prev_ticks));
+            frame_dropped = 0;
+        } else { frame_dropped = 1; }
+        prev_ticks += 33;
     }
+}
+
+int main() {
+    SDL_Init(SDL_INIT_EVERYTHING );
+    auto [window_p, renderer_p] = create_context();
+    play_the_game(renderer_p);
     SDL_Quit();
     return 0;
 }
